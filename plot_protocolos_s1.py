a2c8aa9aadb1f26cb76f10f84450d2a90b09e8cd
pandaconqueso <malenaivnisky@gmail.com>
2017-10-09 18:31:11 -0300

    Σʕﾟᴥﾟﾉʔﾉ

--- plot_protocolos_s1.py
+++ plot_protocolos_s1.py
@@ -24,17 +24,35 @@ for pkt in packets:
 	if 'type' in pkt.fields:
 	        dict_add(d, tipo(pkt.type))
 
-labels = list()
-sizes = list()
+x = list()
+y = list()
 for prot in d:
-	labels.append(prot)
-	sizes.append(d[prot])
+	x.append(prot)
+	y.append(d[prot])
 
 colors = ['palevioletred', 'mediumturquoise', 'gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
  
 # Plot
-plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140, colors=colors)
+# plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140, colors=colors)
  
-plt.axis('equal')
-plt.savefig('protocolos_s1.png', bbox_inches='tight', dpi=200)
+# plt.legend()
+# print(d)
+# plt.savefig('protocolosRed1.png', bbox_inches='tight', dpi=200)
 # plt.show()
+
+porcent = [100.0*i/sum(y) for i in y]
+
+patches, texts = plt.pie(y, colors=colors, shadow=True, startangle=140)
+labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(x, porcent)]
+plt.axis('equal')
+
+sort_legend = True
+if sort_legend:
+    patches, labels, dummy =  zip(*sorted(zip(patches, labels, y),
+                                          key=lambda x: x[2],
+                                          reverse=True))
+
+plt.legend(patches, labels, loc='center left', bbox_to_anchor=(-0.1, 1.),
+           fontsize=8)
+# plt.show()
+plt.savefig('protocolosRed2.png', bbox_inches='tight', dpi=200)
--- plot_red_s2.py
+++ plot_red_s2.py
@@ -2,6 +2,7 @@ import sys
 from scapy.all import rdpcap
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import write_dot
 
 ARP_type = 2054
 
@@ -11,22 +12,25 @@ def dict_add(dic, key):
     else:
         dic[key] = 1
 
-broadcast_src = dict()
+# broadcast_src = dict()
 mensajes = dict()
 nodes = set()
 packets = rdpcap(sys.argv[1])
 for p in packets:
 	if 'type' in p.fields:
 		if p.type == ARP_type:
-			if p.op == 1:
-				# request ARP
-				dict_add(broadcast_src, p.psrc)
-				nodes.add(p.psrc)
-			elif p.op == 2:
-				# reply
-				dict_add(mensajes, (p.psrc, p.pdst))
-				nodes.add(p.psrc)
-				nodes.add(p.pdst)
+			# if p.op == 1:
+			# 	# request ARP
+			# 	dict_add(broadcast_src, p.psrc)
+			# 	nodes.add(p.psrc)
+			# elif p.op == 2:
+			# 	# reply
+			# 	dict_add(mensajes, (p.psrc, p.pdst))
+			# 	nodes.add(p.psrc)
+			# 	nodes.add(p.pdst)
+			dict_add(mensajes, (p.psrc, p.pdst))
+			nodes.add(p.psrc)
+			nodes.add(p.pdst)
 
 # for ip in broadcast_src:
 # 	for n in nodes:
@@ -38,6 +42,18 @@ G.add_nodes_from(nodes)
 for ip1, ip2 in mensajes:
 	G.add_edge(ip1, ip2, weight=mensajes[(ip1, ip2)])
 
-nx.draw_networkx(G)
-# nx.draw_networkx_edge_labels(G,pos=nx.spring_layout(G))
-plt.show()
+# A = nx.to_agraph(G)
+# A.draw('test.png')
+# nx.draw_networkx(G,pos=nx.shell_layout(G))
+# print(mensajes)
+# nx.draw_networkx_edge_labels(G,pos=nx.shell_layout(G))
+# plt.show()
+
+
+
+# nx.draw(G, pos=graphviz_layout(G), node_size=1600, cmap=plt.cm.Blues,
+#         node_color=range(len(G)),
+#         prog='dot')
+# plt.show()
+
+write_dot(G,'graph.dot')
