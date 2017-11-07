6a3c8a16b9bb75f32e114644ab871c0c90299086
pandaconqueso <malenaivnisky@gmail.com>
2017-11-07 20:58:59 -0300

    cmp

--- TP2: Rutas en Internet/traceroute.py
+++ TP2: Rutas en Internet/traceroute.py
@@ -3,6 +3,7 @@ from scapy.all import *
 import sys
 import json
 import math
+from scipy import stats
 
 def main():
 	#Initialize Variables
@@ -22,7 +23,6 @@ def main():
 	global_rtt_acum    = 0
 	global_rtt_average = 0
 	global_standard_deviation = 0
-	global_standard_deviation_acum = 0
 	global_rtt_samples = []
 	global_null_packets_amount = 0.0
 	global_total_packets_amount = 0.0
@@ -31,6 +31,7 @@ def main():
 	
 	# adresses = Counter() #used to check on every iteration the most common Ip adress
 	addresses = dict()
+	final_data = list()
 	
 	result_per_ttl = []
 
@@ -66,54 +67,85 @@ def main():
 	
 		# if rtt_counter > 0 :
 		#   rtt_average = rtt_acum / rtt_counter
+
 		if count > 0:
 			rtt_average = addresses[most_common_ip][0]/addresses[most_common_ip][1]
+			final_data.append([most_common_ip, rtt_average, current_ttl])
 		
-			data_object = {
-				"rtt" : round(rtt_average , 6),
-				# "ip_address" : ' '+str(adresses.most_common(1)[0][0])+' ',
-				"ip_address" : ' '+str(most_common_ip[0][0])+' ',
-				"salto_intercontinental" : False,
-				"hop_num" : current_ttl
+			# data_object = {
+			# 	"rtt" : round(rtt_average , 6),
+			# 	# "ip_address" : ' '+str(adresses.most_common(1)[0][0])+' ',
+			# 	"ip_address" : ' '+str(most_common_ip[0][0])+' ',
+			# 	"salto_intercontinental" : False,
+			# 	"hop_num" : current_ttl
 
 				
-			}
+			# }
 		
 			global_rtt_acum    += rtt_average
 			global_rtt_counter += 1
-			global_rtt_samples.append(rtt_average) 
+			# global_rtt_samples.append(rtt_average) 
 		
 		else:
-			data_object = {
-				"rtt" : None,
-				"ip_address" : None,
-				"salto_intercontinental" : False,
-				"hop_num" : current_ttl
-			}  
-			global_null_packets_amount = global_null_packets_amount +1
+			final_data.append([None, None, current_ttl])
+			# data_object = {
+			# 	"rtt" : None,
+			# 	"ip_address" : None,
+			# 	"salto_intercontinental" : None,
+			# 	"hop_num" : current_ttl
+			# }  
+			global_null_packets_amount += 1
 		
 		
 		global_total_packets_amount = global_total_packets_amount +1  
-		result_per_ttl.append(data_object)
+		# result_per_ttl.append(data_object)
 
 		current_ttl += 1
 		rtt_acum = 0
 		rtt_counter = 0
 		adresses.clear()
+
+	#Calculate rtt diffs
+	for i in range(len(final_data)-1, 0, -1):
+		if final_data[i][1] is None:
+			final_data[i].append(None)
+		else:
+			j = i-1
+			while final_data[j][1] is None and j > 0:
+				j -= 1
+			final_data[i].append(final_data[i][1] - final_data[j][1])	# 3
+
+	final_data.sort(lambda x, y: cmp(x[3], y[3]))
+
+	#Calculate mean
+	sum_ = 0
+	count = 0
+	for data in final_data:
+		if data[2] is not None:
+			sum_ += data[2]
+			count += 1
+	mean = sum_ / count
+
+	#Calculate std dev
+	sum_ = 0
+	for data in final_data:
+		if data[2] is not None:
+			sum_ = (data[2] - mean) ** 2
+	Sdev = math.sqrt(sum_ / (count - 1))
 	
-	#Calculate Overall Average
-	global_rtt_average = global_rtt_acum / global_rtt_counter
-	
-	#Calculate Standard Deviation
-	for rtt_sample in global_rtt_samples :
-	  global_standard_deviation_acum +=  ( (rtt_sample - global_rtt_average )**2 )
-	
-	global_standard_deviation = math.sqrt(global_standard_deviation_acum / global_rtt_counter)
-	
-	#Calculate Z-Value for each rtt
-	for rtt_sample in global_rtt_samples :
-	  z_value = ( rtt_sample - global_rtt_average ) / global_standard_deviation
-	  standard_values.append(z_value)
+	#Calculate absolute value of deviation for each rtt
+	for i in len(final_data):
+		if final_data[i][2] is not None:
+			final_data[i].append(math.abs(final_data[i][2] - mean))		# 4
+
+		# z_value = ( rtt_sample - global_rtt_average ) / global_standard_deviation
+		# standard_values.append(z_value)
+
+	#Calculate tau
+	ppf = stats.t.ppf(0.05, count-2)
+	tau = ppf * (count - 1) / (math.sqrt(count) * math.sqrt(count - 2 + ppf ** 2))
+	tauS = tau * Sdev
+
 	
 	
 	#Write result in File
